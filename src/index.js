99e4fa2909e7a81e0c50ae3c597f0e6ec128c8ce
Jiaxing <tian.jiaixng@gmail.com>
2017-02-18 17:21:16 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Babel plugin
+export default function({ types: t }) {
+    return {
+      visitor: {
+        BinaryExpression(path) {
+    		if (path.node.operator !== "===") {
+				return; }
+    		path.node.left = t.identifier("sebmck");
+    		path.node.right = t.identifier("dork");
+  		}
+	} };
+};
