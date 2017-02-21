138517498a7a660c198795eee08d0016617431e9
John <jfkeis@bu.edu>
2017-02-21 17:39:50 -0500

    added index.js to calc and display primes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-let test = 'Hello';
+// index.js
+
+// @remote
+function calcPrimes(n) {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= n; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= n; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+
+function display(l) {
+   for (i = 0; i < l.length; i++) {
+   		console.log(l[i]);
+   }
+}
+
+
+var n;
+
+if(process.argv[2] > 0){
+	n = process.argv[2];
+}
+else{
+	n = 100;
+}
+
+const l = calcPrimes(n)
+display(l);
