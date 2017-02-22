15a6dd7ef664a77a37ea69c95b61e0a1bca9f678
preranah <preranah@bu.edu>
2017-02-22 17:37:20 -0500

    trying to write aplugin wq

--- src/index.js
+++ src/index.js
@@ -1,40 +1,10 @@
-// index.js
-// calculates and displays prime numbers up to the desired value n
-// accepts user inputted number, if none, uses 100
-// use $ node index.js n
+//plugin
 
-// @remote
-function calcPrimes(n) {
-    var sieve = [], i, j, primes = [];
-    for (i = 2; i <= n; ++i) {
-        if (!sieve[i]) {
-            // i has not been marked -- it is prime
-            primes.push(i);
-            for (j = i << 1; j <= n; j += i) {
-                sieve[j] = true;
-            }
-        }
+export default function({types: t}){
+  return {
+    visitor: {
+      // visitor contents
+      
     }
-    return primes;
-}
-
-function display(l) {
-   for (i = 0; i < l.length; i++) {
-   		console.log(l[i]);
-   }
-}
-
-
-var n;
-
-if(process.argv[2] > 0){
-	n = process.argv[2];
-}
-else{
-	n = 100;
-}
-
-const l = calcPrimes(n)
-display(l);
-
-
+  };
+};
