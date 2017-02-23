eff820c4705c683a2a4fe6ad24938de87c4aaece
preranah <preranah@bu.edu>
2017-02-22 19:54:19 -0500

    initial plugin commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ export default function({types: t}){
   return {
     visitor: {
       // visitor contents
-      
+      Program(path,file) {
+	//path.insertBefore(t.expressionStatement(t.stringLiteral("This is the local version of the app")));	
+	path.unshiftContainer('body', t.expressionStatement(t.stringLiteral('use helloworld')));
+      }      
     }
   };
 };
