0619d6b064bd0f716c6cbb7a8937204ddf1a5fd8
Jiaxing <tian.jiaixng@gmail.com>
2017-03-28 17:29:05 -0400

    Update handler.js

--- S3/handler.js
+++ S3/handler.js
@@ -1,10 +1,9 @@
 'use strict';
 
-
-
 module.exports.fetch = (event, context, callback) => {
-  var AWS = require('aws-sdk');
+  var AWS = require('aws-sdk'); // AWS dependencies
   var s3 = new AWS.S3();
+
   s3.getObject(
     { Bucket: "ec512-demo2", Key: "Google-Rvenue.txt" },
     function (error, data) {
@@ -26,4 +25,14 @@ module.exports.fetch = (event, context, callback) => {
     }
   );
 
+};
+//Draft function need to pass parameter via request promise
+module.exports.calculate = (event. context, callback) => {
+  var list = event.nums;  //list from fetch
+  var arg = 0, total = 0, i;
+  for(i = 0; i < list.length; i++){
+    total += list[i];
+  }
+  arg = total / list.length;
+  callback(null, arg);
 };
