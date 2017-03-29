642e34fa547da086b25bfa263fca93e6f058cc1d
mygty21 <mygty21@bu.edu>
2017-03-29 03:49:28 -0400

    fixed deploy cloud,the sequence is finally right

---
+++ dist/cloud/calcPrimes/calcPrimes.js
@@ -0,0 +1,28 @@
+'use strict;'
+module.exports.calcPrimes = function(event, context, callback)
+
+
+{
+    var obj = event.obj;
+    var n = obj.n;
+    if (n > 0) {
+        // pass
+    } else {
+        n = 100;
+    }
+    var sieve = [],
+        i,
+        j,
+        primes = [];
+    for (i = 2; i <= n; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= n; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    callback(null, primes);
+
+}
---
+++ dist/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var parser = require('./parser');
+var Executor = require('./executor');
+var yargonaut = require('yargonaut').style('blue');
+var yargs = require('yargs');
+
+var yargOptions = {
+    mode: {
+        alias: 'm',
+        describe: 'Mode in which to run the CLI',
+        default: 'extract-cloud',
+        demand: true,
+        choices: ['extract-cloud', 'deploy-cloud', 'prepare-local', 'live'],
+        type: 'string'
+    },
+    'input-dir': {
+        alias: 'i',
+        default: './files',
+        describe: 'Root directory containing the code to be parsed and deployed',
+        demand: true,
+        type: 'string'
+    },
+    'output-dir': {
+        alias: 'd',
+        default: './cloud',
+        describe: 'Target directory where the CLI outputs all the runnable files',
+        demand: true,
+        type: 'string'
+    }
+};
+
+var options = require('yargs').options(yargOptions).example('$0 --mode live --input-dir ./files --output-dir ./cloud', 'Extracts the cloud annotated functions of the current directory, deploys them to AWS and prepares the local files').wrap(null).help().argv;
+
+var executor = new Executor();
+executor.run(options);
