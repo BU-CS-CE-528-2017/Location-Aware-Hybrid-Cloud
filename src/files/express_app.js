8c78d55cd8b16e32e67a1676dc354a775e7e4c59
mygty21 <mygty21@bu.edu>
2017-04-09 22:36:43 -0400

    make it compatible with S3

--- src/files/express_app.js
+++ src/files/express_app.js
@@ -1,5 +1,5 @@
 /* @cloud AWS */
-function getRevenue(obj) {
+function getnumber(obj) {
   var AWS = require('aws-sdk'); // AWS dependencies
   var s3 = new AWS.S3();
 
@@ -8,29 +8,27 @@ function getRevenue(obj) {
   console.log("fetch from" +  bucket);
 
   var nums = []
-  return new Promise(function(resolve, reject) {
-    s3.getObject(
-      { Bucket: bucket, Key: key },
-      function (error, data) {
-        console.log("fetching...");
-        if (error != null) {
-          console.log(error);
-          reject(err);
-          return;
-        } else {
-          let objectData = data.Body.toString('utf-8');
-          var list = objectData.split('\n');
-          for(var i = 0; i < list.length; i++){
-            nums.push(parseFloat(list[i]));
-          }
-          resolve(nums);
+ var result =  new Promise ((resolve,reject) => {
+    s3.getObject({ Bucket: bucket, Key: key }, function (error, data) {
+      console.log("fetching...");
+      if (error != null) {
+        console.log(error);
+        reject(err);
+      } else {
+        var objectData = data.Body.toString('utf-8');
+        var list = objectData.split('\n');
+        for (var i = 0; i < list.length; i++) {
+          nums.push(parseFloat(list[i]));
         }
-      });
+        resolve(nums);
+      }
+    });
   });
+  return Promise.resolve(result);
 }
 
 /* @cloud AWS */
-function average(obj2){
+function calcaverage(obj2){
   //Calculate Average
   var n = obj2.n
   var total = 0, arg = 0;
@@ -46,15 +44,19 @@ const gg = {
   key : 'Google-Rvenue.txt'
 };
 
+var test = {
+  bucket: 'serverless-test-001',
+  key: 'numbers.txt'
+};
 //Express App
 var express = require('express')
 var app = express()
 
 app.get('/avg', (req, res) => {
-  getRevenue(gg).then((result) => {
+  getnumber(test).then((result) => {
     
     var obj2 = {n: result};
-    average(obj2).then((avg) => {
+    calcaverage(obj2).then((avg) => {
       res.send("Fetched: " + result + 
         "<br> Average is: " + avg);
     });
